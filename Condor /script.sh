4b39939dcbd1d07cd011d36890b117e2dffc771b
mishra27 <mishra27@wisc.edu>
2021-04-30 12:56:25 -0500

    Update script.sh

--- Condor /script.sh
+++ Condor /script.sh
@@ -1,49 +1,32 @@
 #!/bin/bash
 
-
 # command to make the PATH work across operating system versions
 export PATH=/bin:$PATH
 
-# Command to enable modules, and then load an appropriate software module
-#. /etc/profile.d/modules.sh
-#module load freesurfer/6
-
-# Command to run your software from the command line
-#wget https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/6.0.0/freesurfer-Linux-centos6_x86_64-stable-pub-v6.0.0.tar.gz
-
-cp /mnt/gluster/mishra27/freesurfer.tar.gz ./
-
+# untar the freesurfer download files and change directory
 tar -xzvf freesurfer.tar.gz
-
-#cp license.txt freesurfer
-
 rm freesurfer.tar.gz
-
 cd freesurfer
 
+# setup freesurfer environments
 export FREESURFER_HOME=`pwd`
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
 cd ..
 
+# set subject directory
 export SUBJECTS_DIR=`pwd`
 
-#find . -name *.nii
-#fullfilename="$(echo *.nill)"
+# tokenizing correct input and outout file names
 fullfilename="$1"
 echo "input file: $fullfilename"
 filename=$(basename "$fullfilename")
 fname="${filename%.*}"
-#fname="$1"
 
-
-echo "fname: $fname"
-echo "full: $fullfilename"
-echo "file: $filename"
-
-#recon-all -sid $fname -i $fullfilename -all
+# runs freesufer recon-all -all command to start processing the MRI .nii images
 recon-all -sid $fname -i $fullfilename -all
 
+# compress the output file so that it can be transfered back to submit server
 tar -czvf $fname.tar.gz $fname
 
 rm -rf freesurfer
